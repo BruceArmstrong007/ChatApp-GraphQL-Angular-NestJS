d2f3c2870d8d6236a726cd07ea3640b6426771d2
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-18 18:16:27 +0530

    Global validation pipe setup

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,11 @@ async function bootstrap() {
     origin: [config.get('CLIENT_URI1'), config.get('CLIENT_URI2')],
   });
   app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
 
   await app.listen(3000);
 }
