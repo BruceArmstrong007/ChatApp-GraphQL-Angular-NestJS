12aaa285728b389228ea575aca6ac4c1d3d7d0d9
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-18 15:59:07 +0530

    set validation schema for env variables, added mongodb module to app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,10 @@ import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { AuthorsResolver } from './resolver/author.resolver';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { MongoDBModule } from '@app/common';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -11,8 +14,36 @@ import { AuthorsResolver } from './resolver/author.resolver';
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
     }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        PORT: Joi.string().required(),
+        CLIENT_URI_1: Joi.string().required(),
+        CLIENT_URI_2: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
+        MAIL_SERVICE: Joi.string().required(),
+        MAIL_HOST: Joi.string().required(),
+        MAIL_PORT: Joi.string().required(),
+        MAIL_EMAIL: Joi.string().required(),
+        MAIL_PASSWORD: Joi.string().required(),
+        MAIL_FROM: Joi.string().required(),
+        VERIFY_TOKEN_TIME: Joi.string().required(),
+        HASH_SALT: Joi.string().required(),
+        COOKIE_EXPIRATION: Joi.string().required(),
+        MONGODB_URI: Joi.string().required(),
+        MONGODB_PASS: Joi.string().required(),
+        MONGODB_NAME: Joi.string().required(),
+        FIREBASE_API_KEY: Joi.string().required(),
+        FIREBASE_PROJECT_ID: Joi.string().required(),
+        FIREBASE_STORAGE_BUCKET: Joi.string().required(),
+      }),
+    }),
+    MongoDBModule,
+    UsersModule
   ],
   controllers: [],
-  providers: [AuthorsResolver],
+  providers: [],
 })
 export class AppModule {}
