683f2b08820902dcac0bad50305621209cce88f6
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-18 17:11:12 +0530

    updated imports in user module

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './schema/users.schema';
+import { User, UserSchema } from './database/user.schema';
+import { UserRepository } from './database/user.repository';
 
 @Module({
-  providers: [UsersResolver, UsersService],
+  providers: [UsersResolver, UsersService, UserRepository],
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  exports: [UsersService],
+  exports: [UsersService, UserRepository],
 })
 export class UsersModule {}
