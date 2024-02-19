e4a70e50b9f1cda09791745be66d49bd3d8e659f
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-19 12:11:06 +0530

    added current-user decorator from prev project and modified according to graphql context

---
+++ server/libs/common/src/decorator/current-user.decorator.ts
@@ -0,0 +1,22 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+export const getCurrentUserByContext = (context: ExecutionContext) => {
+  if (context.getType() === 'http') {
+    return context.switchToHttp().getRequest().user;
+  }
+  const ctx = GqlExecutionContext.create(context);
+  return ctx.getContext().req.user;
+};
+
+export const CurrentUser = createParamDecorator(
+  (_data: unknown, context: ExecutionContext) =>
+    getCurrentUserByContext(context),
+);
+
+export type CurrentUserType = {
+  userID: string,
+  username: string;
+  email: string;
+  verified: boolean;
+};
--- server/libs/common/src/index.ts
+++ server/libs/common/src/index.ts
@@ -1,4 +1,5 @@
 export * from './database/mongodb.module';
 export * from './firebase/firebase.module';
 export * from './validation/password-match.validator';
+export * from './decorator/current-user.decorator';
 export * from './utils/variables';
