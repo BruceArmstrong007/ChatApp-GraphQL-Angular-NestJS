c01cb435a0340d2eed6e5d33a3fe56a6ff1e9578
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-20 10:30:48 +0530

    added PWA to angular project

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,6 +8,7 @@ import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 import { InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
 import { environment } from '../environments/environment';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,16 +17,20 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(ApolloModule),
     provideHttpClient(),
     {
-      provide: APOLLO_OPTIONS,
-      useFactory: (httpLink: HttpLink) => {
-        return {
-          cache: new InMemoryCache(),
-          link: httpLink.create({
-            uri: environment.apiURL,
-          }),
-        };
-      },
-      deps: [HttpLink]
+        provide: APOLLO_OPTIONS,
+        useFactory: (httpLink: HttpLink) => {
+            return {
+                cache: new InMemoryCache(),
+                link: httpLink.create({
+                    uri: environment.apiURL,
+                }),
+            };
+        },
+        deps: [HttpLink]
     },
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 };
