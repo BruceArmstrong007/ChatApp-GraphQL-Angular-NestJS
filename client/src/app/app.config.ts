e602d932f24854c441d0d0b90f3c00e0e0680d65
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-20 10:29:02 +0530

    route params as input binding

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -11,7 +11,7 @@ import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideAnimationsAsync(),
     importProvidersFrom(ApolloModule),
     provideHttpClient(),
