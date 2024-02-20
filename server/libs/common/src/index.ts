8a8d452b5fcfe925b0f3ddac482fa1cab315143d
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-20 09:08:50 +0530

    Added API exception filters

--- server/libs/common/src/index.ts
+++ server/libs/common/src/index.ts
@@ -1,5 +1,6 @@
 export * from './database/mongodb.module';
 export * from './firebase/firebase.module';
+export * from './exception-handler/api-exception.handler';
 export * from './validation/password-match.validator';
 export * from './decorator/current-user.decorator';
 export * from './utils/variables';
