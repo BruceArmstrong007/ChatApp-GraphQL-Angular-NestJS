c8fd99e89cd7031f6915593b4981ec9223460d5e
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-22 09:29:51 +0530

    added register state to register component provider

--- client/src/app/routes/auth/routes/register/register.component.ts
+++ client/src/app/routes/auth/routes/register/register.component.ts
@@ -1,18 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { registerState } from './register.state';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [],
-  template: `
-    <p>
-      register works!
-    </p>
-  `,
+  template: ` <p>register works!</p> `,
   styles: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [registerState],
 })
-class RegisterComponent {
-
-}
+class RegisterComponent {}
 export default RegisterComponent;
