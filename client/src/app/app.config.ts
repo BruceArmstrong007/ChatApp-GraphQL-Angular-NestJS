9c31b281366f2257205de28e61fca0551fdfb9c2
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-27 17:37:01 +0530

    updated app.config after graphQL service implementation

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,3 +1,4 @@
+import { APOLLO_OPTIONS, Apollo, ApolloModule } from 'apollo-angular';
 import {
   ApplicationConfig,
   importProvidersFrom,
@@ -7,29 +8,40 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
-import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
-import { InMemoryCache } from '@apollo/client/core';
-import { HttpLink } from 'apollo-angular/http';
-import { environment } from '../environments/environment';
+import {
+  HttpClient,
+  provideHttpClient,
+  withFetch,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideRouterStore, routerReducer } from '@ngrx/router-store';
 import { provideEffects } from '@ngrx/effects';
-import { provideStore } from '@ngrx/store';
+import { Store, provideStore } from '@ngrx/store';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { CustomRouterStateSerializer } from './state/router/router-serializer';
 import { authFeature } from './state/auth/auth.reducer';
+import { userFeature } from './state/user/user.reducer';
+import { HttpLink } from 'apollo-angular/http';
+import { factoryFn } from './shared/services/apollo/apollo-graphql.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    Apollo,
     provideRouter(routes, withComponentInputBinding()),
     provideAnimationsAsync(),
     importProvidersFrom(ApolloModule, MatSnackBarModule),
-    provideHttpClient(),
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: factoryFn,
+      deps: [Store, HttpLink, HttpClient],
+    },
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
     provideStore({
       router: routerReducer,
       auth: authFeature.reducer,
+      user: userFeature.reducer,
     }),
     provideRouterStore({ serializer: CustomRouterStateSerializer }),
     provideEffects(),
@@ -46,17 +58,5 @@ export const appConfig: ApplicationConfig = {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
     }),
-    {
-      provide: APOLLO_OPTIONS,
-      useFactory: (httpLink: HttpLink) => {
-        return {
-          cache: new InMemoryCache(),
-          link: httpLink.create({
-            uri: environment.apiURL,
-          }),
-        };
-      },
-      deps: [HttpLink],
-    },
   ],
 };
