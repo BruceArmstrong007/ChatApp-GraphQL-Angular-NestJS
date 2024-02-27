ebe639ce4314803395b9448487895c58114e8c88
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-27 17:33:22 +0530

    profile guard updates with currentUser GraphQL API call - Client

--- client/src/app/shared/guards/profile.guard.ts
+++ client/src/app/shared/guards/profile.guard.ts
@@ -1,55 +1,51 @@
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
-import { UserGQL, UserQuery } from '../../../generated-types';
+import { CurrentuserGQL, CurrentuserQuery } from '../../../generated-types';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { map, catchError, of } from 'rxjs';
 import { AlertComponent } from '../components/alert/alert.component';
 import { ApolloQueryResult } from '@apollo/client/core';
 import { Store } from '@ngrx/store';
 import { userActions } from '../../state/user/user.action';
-import { User } from '../utils/types';
+import { User } from '../../state/user/user.model';
 
 export const profileGuard: CanActivateFn = () => {
-  const profile = inject(UserGQL);
+  const currentUser = inject(CurrentuserGQL);
   const store = inject(Store);
   const alert = inject(MatSnackBar);
-  return profile
-    .fetch({
-      FindUserData: {
-        type: '_id',
-        value: '65c318f27676fedc7c78bcf6',
-      },
+  return currentUser.fetch().pipe(
+    map((response: ApolloQueryResult<CurrentuserQuery>) => {
+      if (response?.data?.currentUser) {
+        const temp = response?.data?.currentUser;
+        const user: User = {
+          _id: temp._id,
+          username: temp.username,
+          email: temp.email,
+          bio: temp.bio,
+          name: temp.name,
+          profile: temp.profile,
+          verified: temp.verified,
+          createdAt: temp.createdAt,
+          updatedAt: temp.updatedAt,
+        };
+        store.dispatch(userActions.saveProfile(user));
+        return true;
+      }
+      return false;
+    }),
+    catchError(error => {
+      const errorMsg = error.message;
+      alert.openFromComponent(AlertComponent, {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'right',
+        data: {
+          title: 'API ERROR',
+          message: errorMsg,
+          type: 'ERROR',
+        },
+      });
+      return of(errorMsg);
     })
-    .pipe(
-      map((response: ApolloQueryResult<UserQuery>) => {
-        if (response?.data?.user) {
-          const temp = response?.data?.user;
-          const user: User = {
-            _id: temp._id,
-            username: temp.username,
-            email: temp.email,
-            bio: temp.bio,
-            name: temp.name,
-            profile: temp.profile,
-          };
-          store.dispatch(userActions.saveProfile({ user }));
-          return true;
-        }
-        return false;
-      }),
-      catchError(error => {
-        const errorMsg = error.message;
-        alert.openFromComponent(AlertComponent, {
-          duration: 3000,
-          verticalPosition: 'bottom',
-          horizontalPosition: 'right',
-          data: {
-            title: 'API ERROR',
-            message: errorMsg,
-            type: 'ERROR',
-          },
-        });
-        return of(errorMsg);
-      })
-    );
+  );
 };
