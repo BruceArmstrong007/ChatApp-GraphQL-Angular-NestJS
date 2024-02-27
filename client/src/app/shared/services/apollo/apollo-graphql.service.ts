b7c1a51f2c7c7b64b28598826b7cfaf0bd1289db
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-27 17:36:32 +0530

    Added graphQL APOLLO_OPTIONS factory Function that can handle connections with refetching refresh token if not present

---
+++ client/src/app/shared/services/apollo/apollo-graphql.service.ts
@@ -0,0 +1,105 @@
+import { HttpLink } from 'apollo-angular/http';
+import { ApolloLink, InMemoryCache, split } from '@apollo/client/core';
+import { setContext } from '@apollo/client/link/context';
+import { Store } from '@ngrx/store';
+import { authFeature } from '../../../state/auth/auth.reducer';
+import { environment } from '../../../../environments/environment';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+import { authActions } from '../../../state/auth/auth.action';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+
+export const factoryFn = (
+  store: Store,
+  httpLink: HttpLink,
+  httpClient: HttpClient
+) => {
+  const getHeaders = async () => {
+    return {
+      Accept: 'charset=utf-8',
+    };
+  };
+  const basic = setContext(async () => ({
+    headers: {
+      ...getHeaders(),
+    },
+  }));
+
+  const auth = setContext(async (operation, context) => {
+    const token = store.selectSignal(authFeature.selectAccessToken);
+    if (
+      operation.operationName === 'login' ||
+      operation.operationName === 'register' ||
+      operation.operationName === 'refresh' ||
+      operation.operationName === 'emailVerificationLink' ||
+      operation.operationName === 'emailVerification' ||
+      operation.operationName === 'resetPasswordLink' ||
+      operation.operationName === 'resetPassword'
+    )
+      return {
+        headers: {},
+      };
+    if (!token()) {
+      const res: any = await firstValueFrom(
+        httpClient.post(
+          environment.apiURL,
+          {
+            operationName: 'refresh',
+            query: 'query refresh { refresh { accessToken } }',
+          },
+          {
+            withCredentials: true,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        )
+      );
+      const accessToken = res?.data.refresh.accessToken;
+      if (accessToken) {
+        store.dispatch(authActions.setRefreshToken({ accessToken }));
+        return {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        };
+      }
+    }
+    return {
+      headers: {
+        Authorization: `Bearer ${token()}`,
+      },
+    };
+  });
+
+  const http = ApolloLink.from([
+    basic,
+    auth,
+    httpLink.create({ uri: environment.apiURL, withCredentials: true }),
+  ]);
+
+  const ws = new GraphQLWsLink(
+    createClient({
+      url: environment.wsURL,
+      connectionParams: async () => {
+        return await getHeaders();
+      },
+    })
+  );
+
+  const link = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      );
+    },
+    ws,
+    http
+  );
+
+  return { link, cache: new InMemoryCache() };
+};
