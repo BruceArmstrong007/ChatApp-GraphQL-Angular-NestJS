e8545b71af445cda01d720542170a03582d75f53
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-02-29 17:47:44 +0530

    fix auth bug

--- server/src/auth/strategy/jwt-auth.strategy.ts
+++ server/src/auth/strategy/jwt-auth.strategy.ts
@@ -13,7 +13,8 @@ export class JwtAuthStrategy extends PassportStrategy(Strategy, 'jwt') {
     });
   }
 
-  async validate(payload: any) {
+  async validate(payloadData: any) {
+    const payload = payloadData['_doc'] ?? payloadData;
     if (!payload?.verified)
       throw new UnauthorizedException('User is not verified.');
     return payload;
