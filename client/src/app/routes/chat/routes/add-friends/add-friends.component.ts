7e6a01d16793ad4cd4535efbd8d285d43857db80
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2024-03-13 19:30:05 +0530

    Added Add Friends API and working on UI

--- client/src/app/routes/chat/routes/add-friends/add-friends.component.ts
+++ client/src/app/routes/chat/routes/add-friends/add-friends.component.ts
@@ -1,10 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { addFriends } from './add-friends.state';
+import { UserCardComponent } from '../../../../shared/components/user-card/user-card.component';
 
 @Component({
   selector: 'app-add-friends',
@@ -16,9 +20,12 @@ import { MatSelectModule } from '@angular/material/select';
     MatFormFieldModule,
     MatIconModule,
     MatSelectModule,
+    MatButtonModule,
+    UserCardComponent,
+    NgFor,
   ],
   template: `
-    <mat-card>
+    <mat-card class="max-w-full">
       <mat-card-header>
         <mat-card-title>Add Friends</mat-card-title>
       </mat-card-header>
@@ -35,17 +42,53 @@ import { MatSelectModule } from '@angular/material/select';
             <mat-hint> Username starts with &#64; </mat-hint>
           </mat-form-field>
         </div>
+        <div class="cards-section overflow-y-auto">
+          @for (user of addFriendsState.users(); track user._id) {
+            <app-user-card [user]="user">
+              <ng-container ngProjectAs="buttons">
+                <button
+                  type="button"
+                  mat-mini-fab
+                  (click)="addFriend(user._id)">
+                  <mat-icon class="mat-18">person_add</mat-icon>
+                </button>
+              </ng-container>
+            </app-user-card>
+          } @empty {
+            <div
+              class="w-full h-full flex justify-center items-center font-size-xl">
+              <mat-icon class="mat-18">person_add</mat-icon>
+              <span class="pl-1">Add some friends</span>
+            </div>
+          }
+        </div>
       </mat-card-content>
     </mat-card>
   `,
-  styles: ``,
+  styles: `
+    .cards-section {
+      height: calc(100dvh - 250px);
+    }
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [addFriends],
 })
 class AddFriendsComponent {
+  readonly addFriendsState = inject(addFriends);
   searchUser(target: EventTarget | null) {
     const input = target as HTMLInputElement;
     if (input.value) {
+      this.addFriendsState.searchUsers(input.value);
+    } else {
+      this.addFriendsState.emptyState();
     }
+  }
+
+  addFriend(userID: string | undefined) {
+    if (!userID) return;
+    this.addFriendsState.addFriend({
+      contactID: userID,
+    });
   }
 }
 export default AddFriendsComponent;
